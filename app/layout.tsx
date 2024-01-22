2dccb22694f6add8a1100359da7771d7c2ba838e
tsertic <tsertic5@gmail.com>
2024-01-22 17:46:06 +0100

    nextjs init

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "ODGG",
+  description: "Odvjetnicko društvo Glamuzina & Grošeta",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
