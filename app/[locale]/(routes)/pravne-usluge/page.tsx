99040b30b097bf6040fff185cbd3a1aeead9d230
tsertic <tsertic5@gmail.com>
2024-02-08 11:12:01 +0100

    finished pravne usluge page

--- app/[locale]/(routes)/pravne-usluge/page.tsx
+++ app/[locale]/(routes)/pravne-usluge/page.tsx
@@ -1,12 +1,18 @@
 import { PageHeader } from "@/components/PageHeader";
+import { ServicesAccordion } from "@/components/ServicesAccordion";
+import { SERVICES } from "@/constants";
 import { useTranslations } from "next-intl";
 import React from "react";
 
-const PravneUslugePage = () => {
+const PravneUslugePage = ({ params }: { params: { locale: "en" | "hr" } }) => {
   const t = useTranslations("LegalServices");
+
   return (
     <div>
       <PageHeader>{t("pageTitle")}</PageHeader>
+      <main className="container py-[100px] md:py-[150px]">
+        <ServicesAccordion data={SERVICES[params.locale]} />
+      </main>
     </div>
   );
 };
