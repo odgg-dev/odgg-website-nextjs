f1c353bd7c254df264294f44d0eec5c6d8d206e9
tsertic <tsertic5@gmail.com>
2024-02-17 08:29:27 +0100

    removed unused sanity schemas,updated field validations

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,9 +1,7 @@
 import { type SchemaTypeDefinition } from "sanity";
 
 import blockContent from "./schemas/blockContent";
-import category from "./schemas/category";
-import post from "./schemas/post";
-import author from "./schemas/author";
+
 import language from "./schemas/language";
 import employee from "./schemas/employee";
 import areaOfPractice from "./schemas/areaOfPractice";
@@ -12,9 +10,6 @@ import companyPosition from "./schemas/companyPosition";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [
-    post,
-    author,
-    category,
     blockContent,
     language,
     employee,
